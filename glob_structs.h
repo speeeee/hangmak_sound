843998e1eb76ab89e9466e372e3e9df0de92d1d7
speeeee <snsallay@gmail.com>
2016-10-15 00:25:38 -0400

    prepare new structures for landscape design

--- glob_structs.h
+++ glob_structs.h
@@ -6,15 +6,32 @@
 
 #include <glad/glad.h>
 
+typedef struct GState GState;
+typedef struct Vec3 Vec3;
+
+typedef void (*StateChange)(GState *);
+typedef void (*CollEffect)(GState *, Vec3 /* normal */);
+
 typedef struct { GLfloat cxz; GLfloat cyz; } Camera;
 typedef struct { GLfloat x; GLfloat y; GLfloat z; GLfloat tht; GLfloat phi; } KState;
 
 typedef struct Queue { struct Queue *n; } Queue;
 typedef struct { int t; int act; } Instr;
 
-typedef struct { GLfloat x; GLfloat y; GLfloat z; } Vec3;
+typedef struct { void *a; int sz; } Array; // by context is the type known.
+                                           // the type will be written before upon initialization.
+
+struct Vec3 { GLfloat x; GLfloat y; GLfloat z; };
 typedef struct { GLfloat x; GLfloat y; GLfloat z; Vec3 vel; Vec3 acc; } Player;
-typedef struct { Player pl; Camera ca; KState lk; int t;
-                 Instr *evs; int esz; GLfloat gravity; } GState;
+//typedef struct { Vec3 pos; Vec3 normal; } Plane;
+typedef struct { /* Vec3 */ Array pts; int type; Vec3 normal; } Plane;
+// see 'stage_util.h' for macros corresponding to surface type.
+// assumes only X and Z planes are used.
+// TODO: make array that maps Plane type to CollEffect and an array Surface type to StateChange.
+typedef struct { /* Vec3 */ Array pts; int surf_type; int effect_type; } Surface;
+// see 'stage_util.h' for macros corresponding to effect_type and surf_type.
+typedef struct { /* Surface */ Array sfs; int par; } Stage;
+struct GState { Player pl; Camera ca; KState lk; int t;
+                Instr *evs; int esz; GLfloat gravity; };
 
 #endif
