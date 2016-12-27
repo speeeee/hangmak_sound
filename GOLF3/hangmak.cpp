66226faac2fdb5cc124e40bc01d321fb9d3d08d8
speeeee <snsallay@gmail.com>
2016-12-26 22:26:19 -0500

    extend for all triangles

--- GOLF3/hangmak.cpp
+++ GOLF3/hangmak.cpp
@@ -7,7 +7,15 @@
 #define PI 3.14159265358979
 
 Vec3 v3(float x, float y, float z) { return (Vec3) { x, y, z }; }
-Triangle triangle(float w, float h, Vec3 norm) { return (Triangle) { w, h, norm }; }
+Vec2 v2(float x, float z) { return (Vec2) { x, z }; }
+Triangle triangle(Vec2 a, Vec2 b, Vec2 c, Vec3 norm) { return (Triangle) { a, b, c, norm }; }
+
+Vec2 centroid(Triangle a) { return v2((a.a.x+a.b.x+a.c.x)/3.,(a.a.z+a.b.z+a.c.z)/3.); }
+
+Vec2 vsub2(Vec2 a, Vec2 b) { return v2(a.x-b.x,a.z-b.z); }
+
+Triangle t_centroid(Triangle a) { Vec2 c = centroid(a);
+  return triangle(vsub2(a.a,c),vsub2(a.b,c),vsub2(a.c,c),a.norm); }
 
 Vec3 unit(Vec3 a) { float u = pow(pow(a.x,2)+pow(a.y,2)+pow(a.z,2),0.5);
   return (Vec3) { a.x/u, a.y/u, a.z/u }; }
@@ -19,10 +27,12 @@ float angle(Vec3 a, Vec3 b) { float la = len(a); float lb = len(b);
 Vec3 cross(Vec3 a, Vec3 b) {
   return (Vec3) { a.y*b.z-a.z*b.y, -a.x*b.z+a.z*b.x, a.x*b.y-a.y*b.x }; }
 
+void d_v3(Vec3 a) { glVertex3f(a.x,a.y,a.z); }
+void d_v2(Vec2 a) { glVertex3f(a.x,0,a.z); }
+// requires origin be on centroid.
 void d_tri(Triangle a) { glPushMatrix(); Vec3 c = cross(a.norm,v3(0,1,0));
   glRotatef(180/M_PI*angle(a.norm,v3(0,1,0)),-c.x,-c.y,-c.z);
-  glBegin(GL_TRIANGLES); glVertex3f(-0.5,0,-0.5); glVertex3f(-0.5,0,0.5);
-    glVertex3f(0.5,0,-0.5); glEnd(); glPopMatrix(); }
+  glBegin(GL_TRIANGLES); d_v2(a.a); d_v2(a.b); d_v2(a.c); glEnd(); glPopMatrix(); }
 
 void paint(World *w) { glLoadIdentity(); glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glColor3f(1,0,0); glRotatef(10,-1,0,0); d_tri(w->t[0]); }
@@ -33,7 +43,9 @@ int main() {
   //sf::CircleShape shape(100.f);
   //shape.setFillColor(sf::Color::Blue);
   // TODO: fix angle.
-  World *w = new World(); w->t.push_back(triangle(0,0,v3(0.5,0.5,0)));
+  Triangle a = triangle(v2(0,0),v2(0,1),v2(1,0),v3(0.5,0.5,0));
+  World *w = new World(); //w->t.push_back(triangle(0,0,v3(0.5,0.5,0)));
+  w->t.push_back(t_centroid(a));
   //glRotatef(30,-1,0,0);
   for(bool r = true;r;) {
     sf::Event e; while(window.pollEvent(e)) { if(e.type==sf::Event::Closed) { r = false; } }
--- GOLF3/world.hpp
+++ GOLF3/world.hpp
@@ -5,15 +5,18 @@
 
 typedef struct { float x; float y; float z; } Vec3;
 Vec3 v3(float,float,float);
+typedef struct { float x; float z; } Vec2;
+Vec2 v2(float,float);
 typedef struct { Vec3 acc; Vec3 vel; Vec3 pos; } Projectile;
 
-typedef struct { float w; float h; Vec3 norm; /* Function reaction; */ } Triangle;
-Triangle triangle(float, float, Vec3);
+typedef struct { Vec2 a; Vec2 b; Vec2 c; Vec3 norm; /* Function reaction; */ } Triangle;
+Triangle triangle(Vec2, Vec2, Vec2, Vec3);
 /*
-x-x
+a-b
 |/
-#
+c
 drawn on XZ-plane.
+the three points must be posisitioned such that the centroid of the triangle is the origin.
 */
 
 typedef struct { Projectile p; // main 'ball' that is controlled.
