66db68b85ce33d15e93cd9641ff30919af20a2b2
speeeee <snsallay@gmail.com>
2016-12-30 23:57:59 -0500

    begin work on projectile physics:

---
+++ GOLF3/physics.cpp
@@ -0,0 +1,10 @@
+#include "world.hpp"
+// uses Projectile.
+
+// constant for gravity.  expected to be set to y-val of acceleration multiplied by a number
+//   representing mass.
+#define GRAVITY (-0.01)
+
+// next state of a projectile's motion ignoring collisions.
+Projectile next_state(Projectile a) { Vec3 nv = vadd3(a.acc, a.vel);
+  return (Projectile) { a.acc, nv, vadd3(nv,a.pos) }; }
---
+++ GOLF3/physics.hpp
@@ -0,0 +1,9 @@
+#include "world.hpp"
+// uses Projectile.
+
+// constant for gravity.  expected to be set to y-val of acceleration multiplied by a number
+//   representing mass.
+#define GRAVITY (-0.01)
+
+// next state of a projectile's motion ignoring collisions.
+Projectile next_state(Projectile);
