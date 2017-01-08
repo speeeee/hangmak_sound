e37d18fbc75513d7136fb9874d594d2e70e2134a
speeeee <snsallay@gmail.com>
2017-01-07 22:34:43 -0500

    fix typo that checked plane crossing only with plane wrt first triangle rather than respective triangle

--- GOLF3/physics.cpp
+++ GOLF3/physics.cpp
@@ -10,14 +10,14 @@ Projectile next_state(Projectile a) { Vec3 nv = vadd3(a.acc, a.vel);
 
 // use the vector normal to the test surface to find the correct point along the shell of
 //   the projectile.
-typedef std::function<Projectile(World *, Projectile)> CollisionF;
+typedef std::function<Projectile(World *, Triangle, Projectile)> CollisionF;
 
 void test_collide(World *w, Projectile pp, CollisionF cf) { Projectile res = pp;
   for(int i=0;i<w->t.size();i++) {
-    if(in_triangle(proj_xz(pp.pos),w->t[i])&&pl_side_ball(w->p,pp,w->t[i])) {
-      res = cf(w,res); } } w->p = res; }
+    if(in_triangle(proj_xz(res.pos),w->t[i])&&pl_side_ball(w->p,res,w->t[i])) {
+      res = cf(w,w->t[i],res); } } w->p = res; }
 
-Projectile rigid_elastic(World *w, Projectile pp) {
+Projectile rigid_elastic(World *w, Triangle t, Projectile pp) {
   // pp.vel = pp.vel - 2(pp.vel . normal)normal
-  pp.vel = vsmul(DEGRADE,vsub3(pp.vel,vsmul(2*dot(pp.vel,w->t[0].norm),w->t[0].norm)));
+  pp.vel = vsmul(DEGRADE,vsub3(pp.vel,vsmul(2*dot(pp.vel,t.norm),t.norm)));
   /*w->p.acc = pp.acc; w->p.vel = pp.vel;*/ pp.pos = w->p.pos; return pp; }
