0241f6984bf1bedc6ff3e3840d33a7508875319a
speeeee <snsallay@gmail.com>
2017-01-13 21:47:45 -0500

    change vpts to actual float array (intended for GL_TRIANGLE_STRIP)

--- GOLF3/world.hpp
+++ GOLF3/world.hpp
@@ -62,7 +62,7 @@ typedef struct { Projectile p; // main 'ball' that is controlled.
 typedef std::function<Projectile(World *, Triangle, Projectile)> CollisionF;
 typedef std::function<int(Vec2)> BoundsF;
 
-struct Entity { Vec3 pos; std::vector<Triangle> t; GLuint vpts;
+struct Entity { Vec3 pos; std::vector<Triangle> t; std::vector<float> vpts;
                 BoundsF bf; CollisionF cf; GLuint shader_id; };
-Entity entity(Vec3, std::vector<Triangle>, GLuint, BoundsF, CollisionF, GLuint);
+Entity entity(Vec3, std::vector<Triangle>, std::vector<float>, BoundsF, CollisionF, GLuint);
 #endif
