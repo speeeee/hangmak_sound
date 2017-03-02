48cd96e505f138c361e8456a555428e0cc793571
speeeee <snsallay@gmail.com>
2017-03-02 01:01:42 -0500

    add file for general data storage (like vertices for ball)

---
+++ GOLF3/general_data.hpp
@@ -0,0 +1,9 @@
+#include <cmath>
+#include "world.hpp"
+
+std::vector<float> ball(float rad, int detail) { std::vector<float> ret(detail*6+6);
+  ret[0] = 0; ret[1] = 0; ret[2] = 0;
+  ret[3] = 0; ret[4] = 0; ret[5] = -1; int i;
+  for(float f=0,i=0; f<2*M_PI&&i<ret.size(); f+=2*M_PI/(float)detail,i+=6) {
+    ret[i] = rad*cos(f); ret[i+1] = rad*sin(f); ret[i+2] = 0;
+    ret[i+3] = 0; ret[i+4] = 0; ret[i+5] = 0; } return ret; }
