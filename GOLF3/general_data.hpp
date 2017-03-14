bcc6858bc0b24da0db273fc98660881931be5f4c
speeeee <snsallay@gmail.com>
2017-03-14 15:29:59 -0400

    add header for multiple includes

--- GOLF3/general_data.hpp
+++ GOLF3/general_data.hpp
@@ -1,3 +1,6 @@
+#ifndef GENERAL_DATA_HPP
+#define GENERAL_DATA_HPP
+
 #include <cmath>
 #include "world.hpp"
 
@@ -7,3 +10,4 @@ std::vector<float> ball(float rad, int detail) { std::vector<float> ret(detail*6
   for(float f=0,i=0; f<2*M_PI&&i<ret.size(); f+=2*M_PI/(float)detail,i+=6) {
     ret[i] = rad*cos(f); ret[i+1] = rad*sin(f); ret[i+2] = 0;
     ret[i+3] = 0; ret[i+4] = 0; ret[i+5] = 0; } return ret; }
+#endif
