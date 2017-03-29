df2791ee433983604d2bd6f6b7e71b655041eda2
speeeee <snsallay@gmail.com>
2017-03-29 01:13:18 -0400

    add new function for copying array with different stride

--- GOLF3/general_data.hpp
+++ GOLF3/general_data.hpp
@@ -2,6 +2,7 @@
 #define GENERAL_DATA_HPP
 
 #include <cmath>
+#include <vector>
 #include "world.hpp"
 
 std::vector<float> ball(float rad, int detail) { std::vector<float> ret(detail*6+6);
@@ -10,4 +11,11 @@ std::vector<float> ball(float rad, int detail) { std::vector<float> ret(detail*6
   for(float f=0,i=0; f<2*M_PI&&i<ret.size(); f+=2*M_PI/(float)detail,i+=6) {
     ret[i] = rad*cos(f); ret[i+1] = rad*sin(f); ret[i+2] = 0;
     ret[i+3] = 0; ret[i+4] = 0; ret[i+5] = 0; } return ret; }
+
+std::vector<float> stride_copy(std::vector<float>::iterator a, std::vector<float>::iterator b
+                              ,int stride, int length) {
+  std::vector<float> ret;
+  for(auto i=a;i<b;i+=stride) {
+    for(int j=0;j<length;j++) { ret.push_back(*(i+j)); } } return ret; }
+
 #endif
