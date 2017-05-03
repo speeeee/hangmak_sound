c2937543e7cbafb1fee2ddc833b74c1b552c1b1e
speeeee <snsallay@gmail.com>
2017-05-03 17:56:20 -0400

    complete triangle_strip_surface (not yet tested)

--- PGRG0/polygons.cpp
+++ PGRG0/polygons.cpp
@@ -14,3 +14,26 @@ template <typename T>
 std::vector<T> interpolate(T *a, T *b, int n) {
   std::vector<T> ret(n*n);
   for(int i=0;i<n;i++) { ret.push_back(a[i]); ret.push_back(b[i]); } return ret; }
+// interp_stride: 'n' is meant to be the size of the smaller array.
+template <typename T>
+std::vector<T> interp_stride(std::vector<T> a, std::vector<T> b, int n, int stride) {
+  std::vector<T> ret = a;
+  for(int i=stride;i<n*stride;i+=stride+1) { ret.insert(ret.begin()+i,b[i]); } return ret; }
+
+std::vector<Vec3> interp_tris(std::vector<Vec3> a, int xnsteps, int znsteps) {
+  std::vector<Vec3> ret;
+  for(int i=0;i<znsteps-1;i++) {
+    std::vector<Vec3> tx = interpolate(&a[xnsteps*i],&a[xnsteps*(i+1)],xnsteps);
+    ret.insert(ret.end(),tx.begin(),tx.end()); } return ret; }
+
+std::vector<float> flatten(std::vector<Vec3> a) { std::vector<float> ret;
+  for(int i=0;i<a.size();i++) { ret.push_back(a[i].x); ret.push_back(a[i].y); ret.push_back(a[i].z); }
+  return ret; }
+
+std::vector<float> triangle_strip_surface(FuncXZ f, float xstep, float zstep, int xnsteps
+                                         ,int znsteps, std::vector<Vec3> cols) {
+  std::vector<Vec3> a = interp_tris(triangulate(f,xstep,zstep,xnsteps,znsteps),xnsteps,znsteps);
+  auto a_cols = interp_stride(a,cols,a.size(),1);
+  // TODO: create normals from triangulation.
+  auto a_cols_norms = interp_stride(a_cols,std::vector<Vec3>(a.size(),Vec3(0,0,0)),a.size(),2);
+  return flatten(a_cols_norms); }
--- PGRG0/polygons.hpp
+++ PGRG0/polygons.hpp
@@ -1,3 +1,6 @@
 #include "world.hpp"
 
 std::vector<Vec3> triangulate(FuncXZ f, float xstep, float zstep, int xnsteps, int znsteps); 
+
+std::vector<float> triangle_strip_surface(FuncXZ f, float xstep, float zstep, float xnsteps
+                                         ,float znsteps, std::vector<Vec3> cols);
