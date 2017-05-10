f087bb93453c0c972228d270bb85cc355905a5b0
speeeee <snsallay@gmail.com>
2017-05-09 21:37:01 -0400

    fix major typo in type signature of triangle_strip_surface

--- PGRG0/polygons.hpp
+++ PGRG0/polygons.hpp
@@ -1,6 +1,15 @@
+#ifndef POLYGONS_HPP
+#define POLYGONS_HPP
 #include "world.hpp"
 
-std::vector<Vec3> triangulate(FuncXZ f, float xstep, float zstep, int xnsteps, int znsteps); 
+std::vector<Vec3> triangulate(FuncXZ f, float xstep, float zstep, int xnsteps, int znsteps);
 
-std::vector<float> triangle_strip_surface(FuncXZ f, float xstep, float zstep, float xnsteps
-                                         ,float znsteps, std::vector<Vec3> cols);
+// DONE: make this a template.
+template <typename T>
+std::vector<T> interpolate(T *a, T *b, int n);
+
+std::vector<float> flatten(std::vector<Vec3> a);
+
+std::vector<float> triangle_strip_surface(FuncXZ f, float xstep, float zstep, int xnsteps
+                                         ,int znsteps, std::vector<Vec3> cols);
+#endif
