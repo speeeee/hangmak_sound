454594e19c80ded6bb3e2a68fc8649b21ddc6b08
speeeee <snsallay@gmail.com>
2017-05-13 21:03:05 -0400

    fix almost all errors in triangulation

--- PGRG0/polygons.cpp
+++ PGRG0/polygons.cpp
@@ -9,7 +9,7 @@ std::vector<Vec3> triangulate(FuncXZ f, float xstep, float zstep, int xnsteps, i
 // DONE: make this a template.
 template <typename T>
 std::vector<T> interpolate(T *a, T *b, int n) {
-  std::vector<T> ret(n+n);
+  std::vector<T> ret; // n+n
   for(int i=0;i<n;i++) { ret.push_back(a[i]); ret.push_back(b[i]); } return ret; }
 // interp_stride: 'n' is meant to be the size of the smaller array.
 template <typename T>
@@ -18,10 +18,13 @@ std::vector<T> interp_stride(std::vector<T> a, std::vector<T> b, int n, int stri
   for(int i=0;i<ret.size();i++) { if(!((i+1)%(stride+1))) { ret[i] = b[j++]; }
     else { ret[i] = a[k++]; } } return ret; }
 
+// interp_tris: acts as a sliding window along z-axis.  all internal z-lines are duplicated.
+// TODO: consider using VBOs instead.
 std::vector<Vec3> interp_tris(std::vector<Vec3> a, int xnsteps, int znsteps) {
   std::vector<Vec3> ret;
   for(int i=0;i<znsteps-1;i++) {
     std::vector<Vec3> tx = interpolate(&a[xnsteps*i],&a[xnsteps*(i+1)],xnsteps);
+    //printf("\n"); p_vec3_vec(tx);
     ret.insert(ret.end(),tx.begin(),tx.end()); } return ret; }
 
 std::vector<float> flatten(std::vector<Vec3> a) { std::vector<float> ret;
@@ -35,6 +38,6 @@ std::vector<float> triangle_strip_surface(FuncXZ f, float xstep, float zstep, in
   // TODO: create normals from triangulation.
   auto a_cols_norms = interp_stride(a_cols,std::vector<Vec3>(a.size(),Vec3(0,0,0)),a.size(),2);
   // the following adds degenerate triangle to connect rows.
-  for(int i=2;i<znsteps-1;i++) { int ind = i*xnsteps*3;
-    Vec3 pos = a_cols_norms[ind]; a_cols_norms.insert(a_cols_norms.begin()+ind,pos); }
+  /*for(int i=2;i<znsteps-1;i++) { int ind = i*xnsteps*3;
+    Vec3 pos = a_cols_norms[ind]; a_cols_norms.insert(a_cols_norms.begin()+ind,pos); }*/
   return flatten(a_cols_norms); }
