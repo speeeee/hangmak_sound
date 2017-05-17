edce7f08225f593d902d86fe36c580790b596519
speeeee <snsallay@gmail.com>
2017-05-17 00:23:24 -0400

    fix color vector size, add rotation controls

--- PGRG0/pgrg.cpp
+++ PGRG0/pgrg.cpp
@@ -7,6 +7,7 @@
 #include "matrix.hpp"
 
 #include <cstdio>
+#include <cassert>
 
 #define DEBUG_MODE 1
 #define PI 3.14159265358979323846264338
@@ -23,32 +24,47 @@ void paint(std::vector<Entity> e) {
   d_verts(e[0].vd,GL_TRIANGLE_STRIP); }
 
 Matrix gl_init(sf::Window *window) { glEnable(GL_DEPTH_TEST); glDepthMask(GL_TRUE); glClearDepth(1.f);
-  glDepthFunc(GL_LESS); glEnable(GL_CULL_FACE);
+  glDepthFunc(GL_LESS); glDisable(GL_CULL_FACE);
   glDisable(GL_LIGHTING); /* temporary */ glViewport(0,0,window->getSize().x,window->getSize().y);
   //glMatrixMode(GL_PROJECTION); glLoadIdentity();
   float ratio = window->getSize().x/window->getSize().y;
   //glFrustum(-ratio,ratio,-1.f,1.f,1.f,500.f);
   return frustum_pers(-ratio,ratio,-1.f,1.f,1.f,500.f); }
 
+void handle_input(Matrix *model, Matrix *view, Matrix *projection) {
+  // TODO: make cleaner code.
+  Vec3 axis = Vec3(0,0,0);
+  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+    axis = axis+Vec3(1,0,0); }
+  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+    axis = axis+Vec3(-1,0,0); }
+  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+    axis = axis+Vec3(0,1,0); }
+  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+    axis = axis+Vec3(0,-1,0); }
+  if(!(axis==Vec3(0,0,0))) { *model = rotate(*model,0.01,axis); } }
+
 int main() { sf::ContextSettings settings;
   settings.depthBits = 24; settings.stencilBits = 8; settings.antialiasingLevel = 0;
   settings.majorVersion = 3; settings.minorVersion = 3;
   sf::Window window(sf::VideoMode(800,800), "pgrg", sf::Style::Default, settings);
   window.setVerticalSyncEnabled(true); glewExperimental = GL_TRUE;
+  Matrix projection = gl_init(&window);
   glewInit();
 
   Matrix model = rotate(id_mat(4),PI/4.,Vec3(1,0,0)); Matrix view = translate(id_mat(4),Vec3(0,0,-5.5));
-  Matrix projection = gl_init(&window);
 
   GLuint default_program = create_program(dvs,dfs);
   mvp_set(default_program,model,view,projection);
 
   std::vector<Entity> ei = 
     create_entities({ triangle_strip_surface(fun,0.1,0.1,10,10
-                        ,std::vector<Vec3>(90/* size*2 - xnsteps */,Vec3(1,1,1))) });
+                        ,std::vector<Vec3>(180/* 2*(xsz*ysz - xnsteps) */,Vec3(1,1,1))) });
   ei[0].shader_id = default_program;
 
   int t = 0;
   for(bool r = true;r;t++) {
     sf::Event e; while(window.pollEvent(e)) { if(e.type==sf::Event::Closed) { r = false; } }
+    handle_input(&model,&view,&projection);
+    for(int i=0;i<ei.size();i++) { mvp_set(ei[i].shader_id,model,view,projection); }
     paint(ei); window.display(); } glDeleteVertexArrays(1,&ei[0].vd.vao); return 0; }
